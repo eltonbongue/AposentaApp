b54d933f8a7f1893cb25ff870b3eb12e1abdcba5
Elton Bongue <lourencoelton874@gmail.com>
2025-08-12 23:10:26 +0100

    refactor notifications table

--- database/migrations/2025_08_12_220834_create_notifications_table.php
+++ database/migrations/2025_08_12_220834_create_notifications_table.php
@@ -9,13 +9,16 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
-        Schema::create('notifications', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+  public function up()
+{
+    Schema::create('notifications', function (Blueprint $table) {
+        $table->id();
+        $table->foreignId('user_id')->nullable()->constrained()->onDelete('cascade');
+        $table->text('mensagem');
+        $table->boolean('lida')->default(false);
+        $table->timestamps();
+    });
+}
 
     /**
      * Reverse the migrations.
