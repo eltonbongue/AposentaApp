d0cb9a6e9ced99bbd12c7cfa445d987f8147d93b
Elton Bongue <lourencoelton874@gmail.com>
2025-08-12 23:08:07 +0100

    refactor table

--- database/migrations/2025_08_12_220614_create_goals_table.php
+++ database/migrations/2025_08_12_220614_create_goals_table.php
@@ -9,13 +9,20 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
-        Schema::create('goals', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+    public function up()
+{
+    Schema::create('goals', function (Blueprint $table) {
+        $table->id();
+        $table->foreignId('user_id')->constrained()->onDelete('cascade');
+        $table->enum('tipo', ['anual', 'mensal', 'semanal']);
+        $table->decimal('valor_meta', 15, 2);
+        $table->decimal('saldo_alcancado', 15, 2)->default(0);
+        $table->date('data_inicio')->nullable();
+        $table->date('data_fim')->nullable();
+        $table->timestamps();
+    });
+}
+
 
     /**
      * Reverse the migrations.
